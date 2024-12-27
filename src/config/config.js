0c79d03b9dc10d2b7ffd2c342a01d4c6cc94c303
shubham321-ux <shubham4260shubhu@gmail.com>
2024-12-27 17:08:25 +0530

    first push

--- src/config/config.js
+++ src/config/config.js
@@ -3,14 +3,14 @@ const serverUrl = {
     // uploadUrl: 'http://192.168.1.9:5501/' // Uploads folder URL
 
 
-    // apiUrl: 'https://43.204.172.16:5501/', // API base URL
-    // uploadUrl: 'https://43.204.172.16:5501/uploads' // Uploads folder URL
+    apiUrl: 'https://43.204.172.16:5501/', // API base URL
+    uploadUrl: 'https://43.204.172.16:5501/uploads' // Uploads folder URL
 
 
     
 
-    apiUrl: 'https://imitate.co.in:5501/', // API base URL
-     uploadUrl: 'https://imitate.co.in:5501/uploads' // Uploads folder URL
+    // apiUrl: 'https://imitate.co.in:5501/', // API base URL
+    //  uploadUrl: 'https://imitate.co.in:5501/uploads' // Uploads folder URL
 
 
     // apiUrl: 'https://mgmt-server.onrender.com/', // API base URL
